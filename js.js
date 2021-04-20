f1a6135cddb1b75f43759ea136c5ba64cc2a854e
SergejViktorov <sergej.viktorov1310@gmail.com>
2021-04-20 19:21:35 +0300

    dom

---
+++ js.js
@@ -0,0 +1,5 @@
+console.log(getSubstring('Привет мир', 3));
+console.log(getSubstring('Привет мир', 6));
+console.log(getSubstring('Привет мир', 8));
+console.log(getSubstring('Привет мир', 10));
+console.log(getSubstring('Привет мир', 0));
