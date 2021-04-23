b12e38dfbaffacf4408d614238d723f1fe428c6f
SergejViktorov <sergej.viktorov1310@gmail.com>
2021-04-23 21:17:48 +0300

    7.2

--- js/04-dom-traversal.js
+++
@@ -1,9 +0,0 @@
-/*
- * Свойства «навигации» по DOM-узлам (взять список)
- * http://fecore.net.ua/books/m5ph3r-javascript/module-07/dom-traversal.html
- */
-const navEl = document.querySelector('.site-nav');
-console.log(navEl)
-
-const firstEl = navEl.firstElementChild.nextElementSibling;
-console.log(firstEl)
--- js/08-templating.js
+++
@@ -1,9 +0,0 @@
-/*
- * Свойство innerHTML
- * - чтение
- * - запись
- */
-
-const titleEl = document.querySelector('.title');
-
-titleEl.insertAdjacentHTML('afterbegin', '<a href="#">ссылка </a>')
