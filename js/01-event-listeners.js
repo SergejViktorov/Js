43329196a25933c18c6e65ae0689ed9c75f048bb
SergejViktorov <sergej.viktorov1310@gmail.com>
2021-04-24 14:23:59 +0300

    Update 01-event-listeners.js

--- js/01-event-listeners.js
+++ js/01-event-listeners.js
@@ -13,21 +13,24 @@ const targetBtn = document.querySelector('.js-target-btn');
 const addListenerBtn = document.querySelector('.js-add-listener');
 const removeListenerBtn = document.querySelector('.js-remove-listener');
 
-addListenerBtn.addEventListener('click', event => {
-  console.log(event);
-  console.log('Вешаю слушателя события на целевую кнопку');
 
+
+addListenerBtn.addEventListener('click', event =>{
+  console.log(event);
+  console.log('вешаю слушателя на целевую кнопку')
   targetBtn.addEventListener('click', onTargetBtnClick);
-});
+})
 
-removeListenerBtn.addEventListener('click', event => {
-  console.log(event);
-  console.log('Снимаю слушателя события с целевой кнопки');
-
+removeListenerBtn.addEventListener('click', ()=>{
+  console.log('снимаю слушателя на целевую кнопку');
   targetBtn.removeEventListener('click', onTargetBtnClick);
-});
+ 
+})
+function onTargetBtnClick(event){
+  console.log(event)
+  console.log('click по целевой кнопке')
+}
 
-function onTargetBtnClick(event) {
-  console.log(event);
-  console.log('Клик по целевой кнопке');
+function logMessage(){
+  console.log('клик по целевой кнопке')
 }
